b61b2c9b8b5358a2b9c6ce3a53d3141846efb615
Groza Cristian <kristi9524@gmail.com>
2015-06-26 17:37:09 -0400

    Changed header.

--- packages.el
+++ packages.el
@@ -1,14 +1,14 @@
-;;; packages.el --- evil-html-mode Layer packages File for html-mode
+;;; extensions.el --- Config Layer extensions File for Spacemacs
 ;;
-;; Copyright (c) 2015-2016 Cristian Groza
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
 ;;
-;; Author: Cristian Groza grozac@ymail.com
-;; URL: 
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
-
 ;; List of all packages to install and/or initialize. Built-in packages
 ;; which require an initialization must be listed explicitly in the list.
 (setq evil-html-mode-packages
